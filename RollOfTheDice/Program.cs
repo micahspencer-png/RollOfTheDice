2982bb2b7e8e15f8cdfa4ee15174a589e3ceee5c
micahspencer-png <micahspencer@isu.edu>
2025-10-02 08:07:31 -0600

    Add project files.

---
+++ RollOfTheDice/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RollOfTheDice
+{
+    //Micah Spencer
+    //RCET2265
+    //Fall 25
+    //Roll of the Dice Program
+    //
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Read();
+        }
+    }
+}
